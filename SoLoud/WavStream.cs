68cda88ea6f134a2eda2c94dafedbd70f60a9df9
Влад Кот <vanderofcat@gmail.com>
2024-12-25 21:20:11 +0300

    wavstream wrapper impl

--- SoLoud/WavStream.cs
+++ SoLoud/WavStream.cs
@@ -4,13 +4,13 @@ using SoLoud.Interop;
 
 namespace SoLoud;
 
-public unsafe class Wav(void** handle) : AudioSource(handle), IDisposable {
-    public Wav() : this(Wav_create()) {}
+public unsafe class WavStream(void** handle) : AudioSource(handle), IDisposable {
+    public WavStream() : this(WavStream_create()) {}
 
     public static Wav Load(string filename) {
         var wav = new Wav();
         fixed (char* ptr = filename)
-            ((SOLOUD_ERRORS)Wav_load(wav, (sbyte*) ptr)).ThrowIfError();
+            ((SOLOUD_ERRORS)WavStream_load(wav, (sbyte*) ptr)).ThrowIfError();
         return wav;
     }
 
@@ -20,65 +20,64 @@ public unsafe class Wav(void** handle) : AudioSource(handle), IDisposable {
     /// directly, and the aTakeOwnership parameter to tell SoLoud to free the pointer when the object is being
     /// destroyed.
     /// </summary>
-    public static Wav LoadFromMemory(byte* mem, int length, bool copy = false, bool takeOwnership = false) {
-        var wav = new Wav(); 
-        ((SOLOUD_ERRORS)Wav_loadMemEx(wav, mem, (uint)length, copy?1:0, takeOwnership?1:0)).ThrowIfError();
+    public static WavStream LoadFromMemory(byte* mem, int length, bool copy, bool takeOwnership) {
+        var wav = new WavStream(); 
+        ((SOLOUD_ERRORS)WavStream_loadMemEx(wav, mem, (uint)length, copy?1:0, takeOwnership?1:0)).ThrowIfError();
         return wav;
     }
-
-    public static Wav LoadFromMemory(byte[] array) {
+    public static WavStream LoadFromMemory(byte[] array) {
         fixed (byte* ptr = array)
             return LoadFromMemory(ptr, array.Length, true, false);
     }
 
-    public static Wav LoadFromStream(Stream stream) {
+    public static WavStream LoadFromStream(Stream stream) {
         using var ms = new MemoryStream();
         stream.CopyTo(ms);
         return LoadFromMemory(ms.ToArray());
     }
 
     public void Dispose() {
-        Wav_destroy(this);
+        WavStream_destroy(this);
     }
 
     public override bool Loop {
-        set => Wav_setLooping(this, value?1:0);
+        set => WavStream_setLooping(this, value?1:0);
     }
 
     public override void SetFilter(uint filterId, Filter filter) =>
-        Wav_setFilter(this, filterId, filter);
+        WavStream_setFilter(this, filterId, filter);
 
     public override void SetMinMaxDistance3D(float minDistance, float maxDistance) =>
-        Wav_set3dMinMaxDistance(this, minDistance, maxDistance);
+        WavStream_set3dMinMaxDistance(this, minDistance, maxDistance);
 
     public override void SetAttenuation3D(SoLoudAttenuationModel model, float rolloffFactor) =>
-        Wav_set3dAttenuation(this, (uint) model, rolloffFactor);
+        WavStream_set3dAttenuation(this, (uint) model, rolloffFactor);
 
     public override float DopplerFactor3D {
-        set => Wav_set3dDopplerFactor(this, value);
+        set => WavStream_set3dDopplerFactor(this, value);
     }
 
     public override bool ListenerRelative3D {
-        set => Wav_set3dListenerRelative(this, value?1:0);
+        set => WavStream_set3dListenerRelative(this, value?1:0);
     }
 
     public override bool DistanceDelay3D {
-        set => Wav_set3dDistanceDelay(this, value?1:0);
+        set => WavStream_set3dDistanceDelay(this, value?1:0);
     }
 
     public override void SetCollider3D(AudioCollider collider, int userData) {
-        Wav_set3dColliderEx(this, collider, userData);
+        WavStream_set3dColliderEx(this, collider, userData);
     }
 
     public override AudioAttenuator Attenuator3D {
-        set => Wav_set3dAttenuator(this, value);
+        set => WavStream_set3dAttenuator(this, value);
     }
 
     public override void SetInaudibleBehavior(bool mustTick, bool kill) {
-        Wav_setInaudibleBehavior(this, mustTick?1:0, kill?1:0);
+        WavStream_setInaudibleBehavior(this, mustTick?1:0, kill?1:0);
     }
 
     public override float Volume {
-        set => Wav_setVolume(this, value);
+        set => WavStream_setVolume(this, value);
     }
 }
